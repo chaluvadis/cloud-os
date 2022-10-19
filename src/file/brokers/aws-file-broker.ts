7842a32123de2d37fc167249c71462ab07c94fa2
Evan Coulson <evan.m.coulson@gmail.com>
2022-10-18 18:01:04 -0700

    Added aws file broker

---
+++ src/file/brokers/aws-file-broker.ts
@@ -0,0 +1,46 @@
+import {
+    DeleteObjectCommandOutput,
+    PutObjectCommandOutput,
+    GetObjectCommand,
+    GetObjectCommandOutput,
+    S3Client,
+    DeleteObjectCommand,
+    PutObjectCommand,
+} from '@aws-sdk/client-s3';
+import { Drive } from '../../drive/models/drive';
+import { File } from '../models/file';
+
+export class AWSFileBroker {
+    constructor(private readonly s3Client: S3Client) {}
+
+    getReadableFileContent(
+        drive: Drive,
+        filePath: string
+    ): Promise<GetObjectCommandOutput> {
+        return this.s3Client.send(
+            new GetObjectCommand({
+                Bucket: drive.name,
+                Key: filePath,
+            })
+        );
+    }
+
+    deleteFile(drive: Drive, file: File): Promise<DeleteObjectCommandOutput> {
+        return this.s3Client.send(
+            new DeleteObjectCommand({
+                Bucket: drive.name,
+                Key: file.path,
+            })
+        );
+    }
+
+    putFile(drive: Drive, file: File): Promise<PutObjectCommandOutput> {
+        return this.s3Client.send(
+            new PutObjectCommand({
+                Bucket: drive.name,
+                Key: file.path,
+                Body: file.content,
+            })
+        );
+    }
+}
