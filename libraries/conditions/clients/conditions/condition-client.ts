54fcdd0f873ce4c3ec570a31dd1ca3fefd0b8f1f
Evan Coulson <evan.m.coulson@gmail.com>
2022-10-26 14:12:02 -0700

    Added interfaces to conditions libraries

--- libraries/conditions/clients/conditions/condition-client.ts
+++ libraries/conditions/clients/conditions/condition-client.ts
@@ -1,6 +1,7 @@
 import { ConditionService } from '../../services/foundations/conditions/condition-service';
+import { IConditionClient } from './condition-client.interface';
 
-export class ConditionClient {
+export class ConditionClient implements IConditionClient {
     private readonly conditionService: ConditionService;
 
     constructor() {
--- libraries/conditions/services/foundations/conditions/condition-service.ts
+++ libraries/conditions/services/foundations/conditions/condition-service.ts
@@ -1,4 +1,6 @@
-export class ConditionService {
+import { IConditionService } from './condition-service.interface';
+
+export class ConditionService implements IConditionService {
     isNil(x: unknown): x is null | undefined {
         return x === undefined || x === null;
     }
